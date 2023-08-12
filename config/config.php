fd1476eebdabcdfdc73506e60a168c3a85a83559
akhil <akhilsodvadiya7@gmail.com>
2023-08-12 18:21:02 +0530

    12-08-2023

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+class Config
+{
+    private $HOST = "localhost";
+    private $USERNAME = "root";
+    private $PASSWORD = "";
+    private $DB_NAME = "rnw";
+
+    public function connect()
+    {
+        $res = mysqli_connect(
+            $this->HOST,
+            $this->USERNAME,
+            $this->PASSWORD,
+            $this->DB_NAME,
+        );
+        return $res;
+    }
+
+
+}
+
+?>
